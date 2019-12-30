6f0b496a3117d289c7659cf64f5894128d761135
myle-truong <56028880+myle-truong@users.noreply.github.com>
2019-12-30 21:30:02 +0100

    add form

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <title>Document</title>
+</head>
+<body>
+ <form action="insert.php" method="POST">
+ <h1>Becode Students list </h1>
+  <table>
+    <tr>
+      <td>ID : </td>
+      <td><input type="int" name="ID" ></td>
+    </tr>
+    <tr>
+      <td>first_name : </td>
+      <td><input type="text" name="first_name" ></td>
+    </tr>
+    <tr>
+      <td>last_name : </td>
+      <td><input type="text" name="last_name" ></td>
+    </tr>
+    <tr>
+      <td>username : </td>
+      <td><input type="text" name="username" ></td>
+    </tr>
+    <tr>
+      <td>gender : </td>
+      <td><input type="text" name="gender" ></td>
+    </tr>
+    <tr>
+      <td>linkedin : </td>
+      <td><input type="text" name="linkedin" ></td>
+    </tr>    
+    <tr>
+      <td>github : </td>
+      <td><input type="text" name="github" ></td>
+    </tr> 
+    <tr>
+      <td>email : </td>
+      <td><input type="text" name="email" ></td>
+    </tr> 
+    <tr>
+      <td>preferred_language : </td>
+      <td><input type="text" name="preferred_language" ></td>
+    </tr>
+    <tr>
+      <td>avatar : </td>
+      <td><input type="text" name="avatar" ></td>
+    </tr> 
+    <tr>
+      <td>video : </td>
+      <td><input type="text" name="video" ></td>
+    </tr> 
+    <tr>
+      <td>quote : </td>
+      <td><input type="text" name="quote" ></td>
+    </tr>
+    <tr>
+      <td>quote_author : </td>
+      <td><input type="text" name="quote_author" ></td>
+    </tr>
+    <tr>
+      <td>created_at : </td>
+      <td><input type="text" name="created_at" ></td>
+    </tr>
+  </table> 
+ </form>
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,18 @@
+<?php
+$id = $_POST['ID'];
+$first_name = $_POST['first_name'];
+$first_name = $_POST['first_name'];
+$gender = $_POST['gender'];
+$linkedin = $_POST['linkedin'];
+$github = $_POST['github'];
+$email = $_POST['email'];
+$preferred_language = $_POST['preferred_language'];
+$avatar = $_POST['avatar'];
+$video = $_POST['avatar'];
+$quote = $_POST['quote'];
+$quote_author = $_POST['quote_author'];
+$created_at = $_POST['created_at'];
+
+
+
+?>
