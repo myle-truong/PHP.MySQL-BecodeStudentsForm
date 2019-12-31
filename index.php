c86f16d80d0fbb7dddbf44adb88792103357febc
myle-truong <56028880+myle-truong@users.noreply.github.com>
2019-12-31 09:48:29 +0100

    change style

--- index.php
+++ index.php
@@ -1,72 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <meta http-equiv="X-UA-Compatible" content="ie=edge">
- <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
 </head>
 <body>
- <form action="insert.php" method="POST">
- <h1>Becode Students list </h1>
-  <table>
-    <tr>
-      <td>ID : </td>
-      <td><input type="int" name="ID" ></td>
-    </tr>
-    <tr>
-      <td>first_name : </td>
-      <td><input type="text" name="first_name" ></td>
-    </tr>
-    <tr>
-      <td>last_name : </td>
-      <td><input type="text" name="last_name" ></td>
-    </tr>
-    <tr>
-      <td>username : </td>
-      <td><input type="text" name="username" ></td>
-    </tr>
-    <tr>
-      <td>gender : </td>
-      <td><input type="text" name="gender" ></td>
-    </tr>
-    <tr>
-      <td>linkedin : </td>
-      <td><input type="text" name="linkedin" ></td>
-    </tr>    
-    <tr>
-      <td>github : </td>
-      <td><input type="text" name="github" ></td>
-    </tr> 
-    <tr>
-      <td>email : </td>
-      <td><input type="text" name="email" ></td>
-    </tr> 
-    <tr>
-      <td>preferred_language : </td>
-      <td><input type="text" name="preferred_language" ></td>
-    </tr>
-    <tr>
-      <td>avatar : </td>
-      <td><input type="text" name="avatar" ></td>
-    </tr> 
-    <tr>
-      <td>video : </td>
-      <td><input type="text" name="video" ></td>
-    </tr> 
-    <tr>
-      <td>quote : </td>
-      <td><input type="text" name="quote" ></td>
-    </tr>
-    <tr>
-      <td>quote_author : </td>
-      <td><input type="text" name="quote_author" ></td>
-    </tr>
-    <tr>
-      <td>created_at : </td>
-      <td><input type="text" name="created_at" ></td>
-    </tr>
-  </table> 
- </form>
+  <form action="insert.php" method="POST" >
+  <h1>Becode Students Informations </h1>
+    <section>
+      <table>
+        <h3>Personal information</h3>
+          <tr>
+            <td>Avatar : </td>
+            <td><input type="text" name="avatar" id="avatar" ></td>
+          </tr>
+          <tr>
+            <td>ID : </td>
+            <td><input type="int" name="ID" id="ID" ></td>
+          </tr>
+          <tr>
+            <td>Username : </td>
+            <td><input type="text" name="username" id="username" value=""></td>
+          </tr>
+          <tr>
+            <td>First name : </td>
+            <td><input type="text" name="first_name" id="first_name" value="" ></td>
+          </tr>
+          <tr>
+            <td>Last name : </td>
+            <td><input type="text" name="last_name" id="last_name" value=""></td>
+          </tr>
+          <tr>
+            <td>Gender : </td>
+            <td><input type="text" name="gender" ></td>
+          </tr>
+          <tr>
+            <td>Email : </td>
+            <td><input type="text" name="email" id="email" ></td>
+          </tr>
+          <tr>
+            <td>Preferred_language : </td>
+            <td><input type="text" name="preferred_language" id="linkedin" value="" ></td>
+        </tr> 
+      </table>
+      <table>
+          <h3>Social Network</h3>
+            <tr>
+              <td>Linkedin(url) : </td>
+              <td><input type="text" name="linkedin" id="linkedin" value="" ></td>
+            </tr>    
+            <tr>
+              <td>Github(url) : </td>
+              <td><input type="text" name="github" id="github" value="" ></td>
+            </tr> 
+      </table>
+      <table>
+        <h3>Extral informations</h3>
+        <tr>
+          <label for="quote">Favourite quote: </label>
+          <textarea name="quote" id="quote" cols="15" rows="10"></textarea>
+        </tr>
+        <tr>
+          <td>Quote_author : </td>
+          <td><input type="text" name="quote_author" ></td>
+        </tr>
+        <tr>
+          <td>Favious video : </td>
+          <td><input type="text" name="video" id="video" value="" ></td>
+        </tr>         
+        <tr>
+          <td>Created_at : </td>
+          <td><input type="text" name="created_at" ></td>
+        </tr>
+      </table>
+    </form>
 </body>
 </html>
--- insert.php
+++ insert.php
@@ -14,14 +14,14 @@ try {
 
 $id = $_POST['ID'];
 $first_name = $_POST['first_name'];
-$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
 $gender = $_POST['gender'];
 $linkedin = $_POST['linkedin'];
 $github = $_POST['github'];
 $email = $_POST['email'];
 $preferred_language = $_POST['preferred_language'];
 $avatar = $_POST['avatar'];
-$video = $_POST['avatar'];
+$video = $_POST['video'];
 $quote = $_POST['quote'];
 $quote_author = $_POST['quote_author'];
 $created_at = $_POST['created_at'];
